9fd99cc3083ab87c55f9dc0d6edbbe4266143596
Man Nguyen <minhman2202@gmail.com>
2024-12-26 16:07:30 +0700

    kafka-basics: starting project

---
+++ kafka-beginners-course/kafka-basics/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.apache.kafka:kafka-clients:3.9.0")
+
+    implementation("org.slf4j:slf4j-api:1.7.36")
+    implementation("org.slf4j:slf4j-simple:1.7.36")
+
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ kafka-beginners-course/kafka-basics/src/main/java/org/example/demos/kafka/ProducerDemo.java
@@ -0,0 +1,11 @@
+package org.example.demos.kafka;
+
+public class ProducerDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
